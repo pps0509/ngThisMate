5e0bd4129983ac0d146fe8d6bac44889cad2d359
David Park(Horyang) <ghfid86@gmail.com>
2020-03-30 18:33:40 +0900

    commit for share with my desktop
    commit for share with my desktop!

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+//import { HEROES } from '../mock-heroes';
+import { MessageService } from '../message.service';
+
 
 @Component({
   selector: 'app-heroes',
@@ -6,10 +11,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+  
 
-  constructor() { }
-
-  ngOnInit(): void {
+  selectedHero: Hero; 
+  heroes: Hero[]; 
+  constructor(private heroService: HeroService, private messageService: MessageService) {
+    
   }
 
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add('HeroService: selected Hero id=${hero.id}')
+  }
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
 }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 1, name: 'DeadPool' },
+    { id: 2, name: 'Flash' },
+    { id: 3, name: 'Batman' },
+    { id: 4, name: 'Superman' },
+    { id: 5, name: 'Hawkeye' },
+    { id: 6, name: 'BlackWidow' },
+    { id: 7, name: 'Thor' },
+    { id: 8, name: 'Hulk' },
+    { id: 9, name: 'Captain America' },
+    { id: 10, name: 'Hitgirl' }
+];
