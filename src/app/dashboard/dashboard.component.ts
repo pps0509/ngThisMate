b15432b321a5f0d066d9ea55d6611c438809276a
David Park(Horyang) <ghfid86@gmail.com>
2020-04-01 07:51:02 +0900

    commiting for adding dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { }
 
 @Component({
   selector: 'app-dashboard',
@@ -9,7 +10,13 @@ export class DashboardComponent implements OnInit {
 
   constructor() { }
 
+
   ngOnInit(): void {
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes.slice(1,5))
   }
 
 }
